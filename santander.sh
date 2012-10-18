68d6df86bf98bba07901261e866454f1942e3e80
Joe Cabezas <joe.cabezas@gmail.com>
2012-10-18 02:24:12 -0300

    Added new Service: Banco Santander

---
+++ santander.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#SET SERVICE NAME
+SERVICE_NAME="santander"
+
+#READ PERSONAL DATA
+. "./$SERVICE_NAME.data"
+
+#DEFINE cURL BINARY
+CURL_BIN=curl
+
+COOKIE_FILE=$SERVICE_NAME.cookie
+OUTPUT_FILE=$SERVICE_NAME.html
+
+#LOGIN
+$CURL_BIN \
+	--insecure \
+	--cookie-jar $COOKIE_FILE \
+	--data "rut=$RUT&pin=$PIN" \
+	--location "https://www.santander.cl/transa/cruce.asp" \
+	> /dev/null 2>&1
+
+#FETCHING
+$CURL_BIN \
+	--insecure \
+	--silent \
+	--cookie $COOKIE_FILE \
+	--location "https://www.santander.cl/transa/productos/cm/Ccartola.asp?cta=$CTA" \
+	> $OUTPUT_FILE
+
+#DELETE COOKIE
+rm $COOKIE_FILE
+
+#OUTPUT
+cat $OUTPUT_FILE | awk '/<td valign="MIDDLE" colspan= "2" class=.td_f.>/,/<\/td>/' | awk '{ for(i=1; i<=NF; i++) if($i ~/^[0-9]+/) {print $i} }'
